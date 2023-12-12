eef7d360718aad67f2e7c87d85ea808189858f61
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2023-12-13 01:42:23 +0600

    Update Database Server Creation.sh

--- Database Server Creation.sh
+++ Database Server Creation.sh
@@ -3,7 +3,7 @@
 # Step 3 : Create a NAT gateway and attach it with a public sublic subnet where internet gateway is routed.<br/>
 # Step 4 : Create a route table and edit route adding nat gateway in the list. <br/>
 # Step 5 : Create a EC2 instance and name it DB-Server. <br/> 
-# Step 6 : Access a public server under the same vpc whuch the DB-Server is in. <br/>
+# Step 6 : Access a public server under the same vpc where the DB-Server is in. <br/>
 # Step 7 : Save the key of the private server. <br/>
 # Step 8 : Change the mod of the access key to 400. <br/>
 # Step 9 : Access the private server with the key through SSH. <br/>
